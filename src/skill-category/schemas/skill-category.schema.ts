fc32cd1ab062b23f24420570b1c144445cdeaf14
H-isaac23 <dev.isaac23@gmail.com>
2024-01-27 11:13:36 +0800

    add skill-category resource

---
+++ src/skill-category/schemas/skill-category.schema.ts
@@ -0,0 +1,20 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+import * as mongoose from 'mongoose';
+import { User } from 'src/user/schemas/user.schema';
+
+export type SkillCategoryDocument = HydratedDocument<SkillCategory>;
+
+@Schema()
+export class SkillCategory {
+  @Prop({ required: true })
+  skill_category_name: string;
+
+  @Prop()
+  skill_category_description: string;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  user: User;
+}
+
+export const SkillCategorySchema = SchemaFactory.createForClass(SkillCategory);
